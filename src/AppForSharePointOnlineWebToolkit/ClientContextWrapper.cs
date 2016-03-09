e4cf89ed609ace7ae139f1627a92e62fd74694b1
Justin Yoo <justin.yoo@aliencube.com>
2016-03-09 17:04:46 +1100

    Update ClientContextWrapper

--- src/AppForSharePointOnlineWebToolkit/ClientContextWrapper.cs
+++ src/AppForSharePointOnlineWebToolkit/ClientContextWrapper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net;
 using System.Threading.Tasks;
 
 using Microsoft.SharePoint.Client;
@@ -35,6 +36,22 @@ namespace AppForSharePointOnlineWebToolkit
         public ClientContext ContextInstance { get; set; }
 
         /// <summary>
+        /// Gets the <see cref="Web"/> instance.
+        /// </summary>
+        public Web Web
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.Web;
+            }
+        }
+
+        /// <summary>
         /// Gets the <see cref="Site"/> instance.
         /// </summary>
         public Site Site
@@ -51,9 +68,9 @@ namespace AppForSharePointOnlineWebToolkit
         }
 
         /// <summary>
-        /// Gets the <see cref="Web"/> instance.
+        /// Gets the <see cref="RequestResources"/> instance.
         /// </summary>
-        public Web Web
+        public RequestResources RequestResources
         {
             get
             {
@@ -62,8 +79,472 @@ namespace AppForSharePointOnlineWebToolkit
                     throw new InvalidOperationException();
                 }
 
-                return this.ContextInstance.Web;
+                return this.ContextInstance.RequestResources;
             }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether form digest handling enabled or not.
+        /// </summary>
+        public bool FormDigestHandlingEnabled
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.FormDigestHandlingEnabled;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.FormDigestHandlingEnabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Version"/> instance.
+        /// </summary>
+        public Version ServerVersion
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.ServerVersion;
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL.
+        /// </summary>
+        public string Url
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.Url;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the application name.
+        /// </summary>
+        public string ApplicationName
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.ApplicationName;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.ApplicationName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the client tag.
+        /// </summary>
+        public string ClientTag
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.ClientTag;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.ClientTag = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to disable return value cache or not.
+        /// </summary>
+        public bool DisableReturnValueCache
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.DisableReturnValueCache;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.DisableReturnValueCache = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to validate on client or not.
+        /// </summary>
+        public bool ValidateOnClient
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.ValidateOnClient;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.ValidateOnClient = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the authentication mode.
+        /// </summary>
+        public ClientAuthenticationMode AuthenticationMode
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.AuthenticationMode;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.AuthenticationMode = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the forms authentication login info.
+        /// </summary>
+        public FormsAuthenticationLoginInfo FormsAuthenticationLoginInfo
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.FormsAuthenticationLoginInfo;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.FormsAuthenticationLoginInfo = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the credentials.
+        /// </summary>
+        public ICredentials Credentials
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.Credentials;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.Credentials = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the web request executor factory.
+        /// </summary>
+        public WebRequestExecutorFactory WebRequestExecutorFactory
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.WebRequestExecutorFactory;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.WebRequestExecutorFactory = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the pending request.
+        /// </summary>
+        public ClientRequest PendingRequest
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.PendingRequest;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the context has pending request or not.
+        /// </summary>
+        public bool HasPendingRequest
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.HasPendingRequest;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the tag.
+        /// </summary>
+        public object Tag
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.Tag;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.Tag = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the request timeout.
+        /// </summary>
+        public int RequestTimeout
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.RequestTimeout;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.RequestTimeout = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the static objects.
+        /// </summary>
+        public Dictionary<string, object> StaticObjects
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.StaticObjects;
+            }
+        }
+
+        /// <summary>
+        /// Gets the server schema version.
+        /// </summary>
+        public Version ServerSchemaVersion
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.ServerSchemaVersion;
+            }
+        }
+
+        /// <summary>
+        /// Gets the server library version.
+        /// </summary>
+        public Version ServerLibraryVersion
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.ServerLibraryVersion;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the request schema version.
+        /// </summary>
+        public Version RequestSchemaVersion
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.RequestSchemaVersion;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.RequestSchemaVersion = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the trace correlation Id.
+        /// </summary>
+        public string TraceCorrelationId
+        {
+            get
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return this.ContextInstance.TraceCorrelationId;
+            }
+
+            set
+            {
+                if (this.ContextInstance == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                this.ContextInstance.TraceCorrelationId = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="FormDigestInfo"/> instance.
+        /// </summary>
+        /// <returns>Returns the <see cref="FormDigestInfo"/> instance.</returns>
+        public FormDigestInfo GetFormDigestDirect()
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.ContextInstance.GetFormDigestDirect();
         }
 
         /// <summary>
@@ -91,6 +572,172 @@ namespace AppForSharePointOnlineWebToolkit
             }
 
             return Task.Factory.StartNew(this.ExecuteQuery);
+        }
+
+        /// <summary>
+        /// Casts the <see cref="ClientObject"/> instance to the given type.
+        /// </summary>
+        /// <param name="obj"><see cref="ClientObject"/> instance.</param>
+        /// <typeparam name="T">Type to convert.</typeparam>
+        /// <returns>Returns the converted type.</returns>
+        public T CastTo<T>(ClientObject obj) where T : ClientObject
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return this.ContextInstance.CastTo<T>(obj);
+        }
+
+        /// <summary>
+        /// Casts the <see cref="ClientObject"/> instance to the given type asynchronously.
+        /// </summary>
+        /// <param name="obj"><see cref="ClientObject"/> instance.</param>
+        /// <typeparam name="T">Type to convert.</typeparam>
+        /// <returns>Returns the converted type.</returns>
+        public Task<T> CastToAsync<T>(ClientObject obj) where T : ClientObject
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            var result = default(T);
+            Task.Factory.StartNew(() => { result = this.CastTo<T>(obj); });
+            return Task.FromResult(result);
+        }
+
+        /// <summary>
+        /// Adds query into the context.
+        /// </summary>
+        /// <param name="query"><see cref="ClientAction"/> instance for query.</param>
+        public void AddQuery(ClientAction query)
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            this.ContextInstance.AddQuery(query);
+        }
+
+        /// <summary>
+        /// Adds query into the context asynchronously.
+        /// </summary>
+        /// <param name="query"><see cref="ClientAction"/> instance for query.</param>
+        /// <returns>Returns the <see cref="Task"/>.</returns>
+        public Task AddQueryAsync(ClientAction query)
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            return Task.Factory.StartNew(() => { this.AddQuery(query); });
+        }
+
+        /// <summary>
+        /// Adds query Id and result object into the context.
+        /// </summary>
+        /// <param name="id">Query Id.</param>
+        /// <param name="obj">Result object.</param>
+        public void AddQueryIdAndResultObject(long id, object obj)
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            this.ContextInstance.AddQueryIdAndResultObject(id, obj);
+        }
+
+        /// <summary>
+        /// Adds query Id and result object into the context asynchronously.
+        /// </summary>
+        /// <param name="id">Query Id.</param>
+        /// <param name="obj">Result object.</param>
+        /// <returns>Returns the <see cref="Task"/>.</returns>
+        public Task AddQueryIdAndResultObjectAsync(long id, object obj)
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return Task.Factory.StartNew(() => { this.AddQueryIdAndResultObject(id, obj); });
+        }
+
+        /// <summary>
+        /// Parses object from the JSON string.
+        /// </summary>
+        /// <param name="json">JSON string value.</param>
+        /// <returns>Returns the object parsed.</returns>
+        public object ParseObjectFromJsonString(string json)
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
+
+            return this.ContextInstance.ParseObjectFromJsonString(json);
+        }
+
+        /// <summary>
+        /// Parses object from the JSON string asynchronously.
+        /// </summary>
+        /// <param name="json">JSON string value.</param>
+        /// <returns>Returns the object parsed.</returns>
+        public Task<object> ParseObjectFromJsonStringAsync(string json)
+        {
+            if (this.ContextInstance == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
+
+            object result = null;
+            Task.Factory.StartNew(() => { result = this.ParseObjectFromJsonString(json); });
+            return Task.FromResult(result);
         }
 
         /// <summary>
