d3c4c0cc00f11e1fa53354be9dc68925b3820f98
Justin Yoo <justin.yoo@aliencube.com>
2016-06-06 23:48:14 +1000

    Add ASP.NET Core RC2 sample

---
+++ samples/SpoConfigRc2Sample/src/SpoConfigRc2Sample.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AppForSharePointOnlineWebToolkit;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpoConfigRc2Sample.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var helper = new ClientContextHelper();
+            var client = helper.CreateAppOnlyClientContext("https://[TENANT_NAME]-admin.sharepoint.com");
+            client.Dispose();
+            return this.View();
+        }
+
+        public IActionResult About()
+        {
+            this.ViewData["Message"] = "Your application description page.";
+
+            return this.View();
+        }
+
+        public IActionResult Contact()
+        {
+            this.ViewData["Message"] = "Your contact page.";
+
+            return this.View();
+        }
+
+        public IActionResult Error()
+        {
+            return this.View();
+        }
+    }
+}
